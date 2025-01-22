108cfe3774aa6b0e57ffafc54a9b2a6367f7b527
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-01-22 12:49:50 +0100

    Update the data model to generate uique id for each expense created using the uuid pckage

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,10 +1,13 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
 class Expense {
-  const Expense({
-    required this.id,
+  Expense({
     required this.title,
     required this.amount,
     required this.date,
-  });
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
