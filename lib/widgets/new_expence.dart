d0f81b9c5eb4b53f25443e8c7b1d5d061c55d07c
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-01-27 21:38:16 +0100

    Implement the functionality to submit newly created expenses

--- lib/widgets/new_expence.dart
+++ lib/widgets/new_expence.dart
@@ -29,6 +29,16 @@ class _NewExpenceState extends State<NewExpence> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      // show error message
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -113,10 +123,7 @@ class _NewExpenceState extends State<NewExpence> {
                 child: const Text('Cancle'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
