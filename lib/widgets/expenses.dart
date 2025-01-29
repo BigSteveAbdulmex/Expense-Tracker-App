2a2189e56181ea090aa81e561a44e46ebb200fa2
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-01-29 20:55:51 +0100

    Added safe area to the modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -32,6 +32,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenceOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpence(onAddExpense: _addExpense),
